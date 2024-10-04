95c5090e7e7c17455ce660abf2ef03867585e091
Emon Khan <codersemon@gmail.com>
2024-10-04 18:46:49 +0600

    project source code github link added to the theme configuration

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
-import Link from "next/link";
 
 const themeConfig = {
   logo: (
     <>
-      <Image src="/images/emonkhan.me.png" width={220} height={100} alt="Logo" />
+      <Image
+        src="/images/emonkhan.me.png"
+        width={220}
+        height={100}
+        alt="Logo"
+      />
     </>
   ),
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/codersemon/notesdoc",
   },
   backgroundColor: {
-    dark: '11, 17, 32',
+    dark: "11, 17, 32",
   },
   footer: {
-    content: 'Develop by Emon Khan'
-  }
+    content: "Develop by Emon Khan",
+  },
 };
 export default themeConfig;
