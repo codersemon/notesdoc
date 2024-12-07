e2777e302902c8fa8cec1df068a208964a1886cf
Emon Khan <codersemon@gmail.com>
2024-12-07 17:14:44 +0600

    some note added in nextjs routing

--- pages/nextjs/_meta.js
+++ pages/nextjs/_meta.js
@@ -1,13 +1,7 @@
-const nextjsRoutes = {
+const nextjsRootRoutes = {
   index: "Introduction",
   installation: "Installation",
   "project-structure": "Project Structure",
-  "routes-pages-layout": "Routes, Pages & Layouts",
-  "use-client": "Client Side Render",
-  "linking-navigating": "Linking and Navigating",
-  "loading-streaming": "Loading and Streaming",
-  "error-handle": "Error Handle",
-  "route-groups": "Route Groups",
-  "dynamic-routes": "Dynamic Routes",
+  routing: "Routing",
 };
-export default nextjsRoutes;
+export default nextjsRootRoutes;
---
+++ pages/nextjs/routing/_meta.js
@@ -0,0 +1,15 @@
+const nextjsRoutes = {
+  "routes-pages-layout": "Routes, Pages & Layouts",
+  "use-client": "Client Side Render",
+  "linking-navigating": "Linking and Navigating",
+  "loading-streaming": "Loading and Streaming",
+  "error-handle": "Error Handle",
+  "route-groups": "Route Groups",
+  "dynamic-routes": "Dynamic Routes",
+  "parallel-routes": "Parallel Routes",
+  "intercepting-routes": "Intercepting Routes",
+  route: "Route Handlers",
+  middleware: "Middleware",
+  internationalization: "Internationalization",
+};
+export default nextjsRoutes;
